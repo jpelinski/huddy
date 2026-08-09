b074c0d5c251467f8510d566bc5f1399b98e6740
Jakub Peliński <pelinski.jakub@gmail.com>
2026-08-09 12:11:16 +0200

    fix: loadfile path changed

--- electron/main.ts
+++ electron/main.ts
@@ -34,7 +34,7 @@ function createWindow() {
         win.loadURL("http://localhost:5173");
         // win.webContents.openDevTools()
     } else {
-        win.loadFile(path.join(__dirname, "../dist/index.html"));
+        win.loadFile(path.join(__dirname, "../../dist/index.html"));
     }
 }
 
